0adf88fd0997e8f6eb922d8dd7e0046c6e4e6823
Youta1218 <youtan.yamamoto@gmail.com>
2023-08-16 20:17:40 +0900

    08-5課題2-5

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
+        <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1 class="title">編集画面</h1>
         <div class="content">
-            <form action="?posts/{{ $post->id }}" method="POST">
+            <form action="/posts/{{ $post->id }}" method="POST">
                 @csrf
                 @method('put')
             <div class="content_title">
@@ -18,7 +18,8 @@
                 <input type='text' name='post[title]' value="{{ $post->title}}">
             </div>
             <div class="content_body">
-                <h2>タイトル</h2>
+                <h2>本文</h2>
+                <!--<textarea name="post[body]" placeholder='post[body]' value=""></textarea>-->
                 <input type='text' name='post[body]' value="{{ $post->body}}">
             </div>
             <input type="submit" value="保存">
